ba9044bc329da56f548526f37109c5b0195da5c2
Salvatore Di Costanzo <dicostanzosalvatore@yahoo.it>
2020-11-04 09:59:32 +0100

    Initial commit

---
+++ src/main/java/com/example/POCBatchZip/PocBatchZipApplication.java
@@ -0,0 +1,13 @@
+package com.example.POCBatchZip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocBatchZipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocBatchZipApplication.class, args);
+	}
+
+}
